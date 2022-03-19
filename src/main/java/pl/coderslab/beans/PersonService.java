50bf205bc082d47673e34ce338dc5a8e667f6660
MateuszChmielecki <ma.teusz.gsi@gmail.com>
2022-03-19 17:34:21 +0100

    Initial commit

---
+++ src/main/java/pl/coderslab/beans/PersonService.java
@@ -0,0 +1,20 @@
+package pl.coderslab.beans;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+
+public class PersonService {
+    private final PersonRepository personRepo;
+
+    public PersonService(PersonRepository personRepo) {
+        this.personRepo = personRepo;
+    }
+
+    public PersonRepository getPersonRepo() {
+        return personRepo;
+    }
+
+
+}
